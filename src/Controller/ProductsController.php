0c89e9dd03f6e9cbee6365b58d595b24a2b7f683
Séverine <severine.coute@laposte.net>
2021-04-15 23:03:24 +0200

    création page produit

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Controller;
+
+class ProductsController extends AbstractController
+{
+    // Display list of products
+   
+    public function index()
+    {
+    return $this->twig->render('Products/index.html.twig');
+    }
+}
---
+++ src/Model/ProductsManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+class ProductsManager extends AbstractManager
+{
+    public const TABLE = 'products';
+
+}
+
+// on veut lister /products/index 
