744587afcb4b2d7b6a11e37bd4fa463d9fa2cf7b
Séverine <severine.coute@laposte.net>
2021-04-16 09:28:23 +0200

    debug erreurs

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -1,14 +1,11 @@
 <?php
-
-
 namespace App\Controller;
 
 class ProductsController extends AbstractController
 {
     // Display list of products
-   
     public function index()
     {
-    return $this->twig->render('Products/index.html.twig');
+        return $this->twig->render('Products/index.html.twig');
     }
 }
--- src/Model/ProductsManager.php
+++ src/Model/ProductsManager.php
@@ -5,7 +5,4 @@ namespace App\Model;
 class ProductsManager extends AbstractManager
 {
     public const TABLE = 'products';
-
 }
-
-// on veut lister /products/index 
